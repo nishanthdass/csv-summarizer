5fb59a6b4f3d50f399da003d058e985519550cd7
Nishanth Dass <nishanthdass@gmail.com>
2025-01-05 19:02:32 -0500

    Mark Langchain Docs as if is_image

--- backend/qa_with_postgres/pdf_processing.py
+++ backend/qa_with_postgres/pdf_processing.py
@@ -7,6 +7,7 @@ from rich import print as rprint
 import re
 
 # Initialize output dictionary
+book_array = []
 page_obj = {}
 
 # Intialize variables for character text splitters
@@ -42,8 +43,8 @@ markdown_splitter = MarkdownHeaderTextSplitter(
 )
 
 recur_text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    chunk_overlap=40,
+    chunk_size=200,
+    chunk_overlap=30,
     length_function=len,
     separators=separators,
     is_separator_regex=True,
@@ -69,7 +70,6 @@ page_obj['metadata'] = {
     'page': info['metadata']['page'],
     'toc_items': info['toc_items'],}
 page_obj['images'] = info['images']
-page_obj['image_files'] = ""
 
 # Variables for processing
 text = output[0]['text']
@@ -94,7 +94,7 @@ for line in text.split('\n'):
 
     for word in line.split(' '):
         if word_count >= len(words) and word.strip():
-            page_obj['image_files'] += word + ' '
+            build_paragraph.append(word)
         elif word in headers:
             build_paragraph.append(word)
         elif word.strip():
@@ -113,18 +113,23 @@ for line in page_lines:
 last_meta = None
 line_count = 0
 
-# Insert line numer into metadata
+# Insert line number, page number and is_image into metadata
 for line in md_lines:
+    match = re.match(r'!\[\]\([^)]*\)', line.page_content)
     line_count += 1
     if len(line.metadata.keys()) > 0:
         line.metadata['line_number'] = line_count
+        line.metadata['page_number'] = page_obj['metadata']['page']
+        line.metadata['is_image'] = match
         last_meta = line.metadata.copy()
     else:
         if last_meta:
             line.metadata = last_meta.copy()
         line.metadata['line_number'] = line_count
+        line.metadata['page_number'] = page_obj['metadata']['page']
+        line.metadata['is_image'] = match
 
+
+rprint(md_lines)
 # chunk page_content larger that 200 words
 chunks = recur_text_splitter.split_documents(md_lines)
-
-rprint(chunks)
